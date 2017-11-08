a402f98cd9728d9f51e25917efad96c2d34a2aca
aaronjv <aaronjv@g.clemson.edu>
2017-11-07 19:10:08 -0500

    Google Maps ish and added Paw Hammer font

--- app/src/main/java/com/clemsonrun/runningapp/MainActivity.java
+++ app/src/main/java/com/clemsonrun/runningapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.clemsonrun.runningapp;
 
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.appindexing.Action;
+import com.google.firebase.appindexing.FirebaseUserActions;
+import com.google.firebase.appindexing.Indexable;
+import com.google.firebase.appindexing.builders.Actions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/pawHammer.otf");
+        TextView tv = findViewById(R.id.titleText);
+        tv.setTypeface(tf);
     }
 
     @Override
@@ -27,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        //updateUI(currentUser);
     }
 
-    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    /*mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
         @Override
         public void onComplete(@NonNull Task<AuthResult> task) {
             if (task.isSuccessful()) {
@@ -68,5 +77,5 @@ public class MainActivity extends AppCompatActivity {
 
             // ...
         }
-    });
+    });*/
 }
