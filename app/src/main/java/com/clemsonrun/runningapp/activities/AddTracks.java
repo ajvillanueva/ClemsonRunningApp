96240807430f7959b406ac56d380cdcc524852fa
aaronjv <aaronjv@g.clemson.edu>
2017-11-09 20:03:00 -0500

    A C T I V I T I E S

---
+++ app/src/main/java/com/clemsonrun/runningapp/activities/AddTracks.java
@@ -0,0 +1,15 @@
+package com.clemsonrun.runningapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clemsonrun.runningapp.R;
+
+public class AddTracks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tracks);
+    }
+}
